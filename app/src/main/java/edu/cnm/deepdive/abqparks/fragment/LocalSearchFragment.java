d0963166ddd9e297107301f76f5e18c356070bfd
Michael Lacey <laceymwes@gmail.com>
2018-07-31 11:00:13 -0600

    - Begin location permissions check/request logic in LocalSearchFragment.

--- app/src/main/java/edu/cnm/deepdive/abqparks/fragment/LocalSearchFragment.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/fragment/LocalSearchFragment.java
@@ -1,16 +1,31 @@
 package edu.cnm.deepdive.abqparks.fragment;
 
+import android.Manifest.permission;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import edu.cnm.deepdive.abqparks.R;
 
 
 public class LocalSearchFragment extends Fragment {
 
-  public static final String POSTAL_KEY = "postal_key";
+  private static final String POSTAL_KEY = "postal_key";
+  private static final int FINE_LOCATION_REQUEST_CODE = 1;
+
+  private LocationManager locationManager;
+  private double deviceLat;
+  private double deviceLng;
 
   public LocalSearchFragment() {
     // Required empty public constructor
@@ -25,6 +40,13 @@ public class LocalSearchFragment extends Fragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+    if (ContextCompat.checkSelfPermission(getActivity(), permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      requestPermissions();
+    } else {
+      getLocation();
+    }
   }
 
   @Override
@@ -33,6 +55,47 @@ public class LocalSearchFragment extends Fragment {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_local_search, container, false);
   }
+
+  private void requestPermissions() {
+    // Explanation required
+    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+        permission.ACCESS_FINE_LOCATION)) {
+      // TODO: implement asynchronous permission prompt with explanation
+    } else {
+      ActivityCompat.requestPermissions(getActivity(),
+          new String[]{permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    switch (requestCode) {
+      case FINE_LOCATION_REQUEST_CODE:
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          getLocation();
+        } else {
+          Toast.makeText(getActivity(), "Cannot locate local parks without location permissions.",
+              Toast.LENGTH_SHORT).show();
+        }
+    }
+  }
+
+  private void updateMap() {
+
+  }
+
+  private void getLocation() {
+    @SuppressLint("MissingPermission") // getLocation is only called if permission has been granted
+    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    if (location != null) {
+      deviceLat = location.getLatitude();
+      deviceLng = location.getLongitude();
+    } else {
+      Toast.makeText(getActivity(), "Unable to determine device location.", Toast.LENGTH_SHORT).show();
+    }
+  }
+
 
   // TODO: save postal code to recover from fragment/activity destruction.
   @Override
