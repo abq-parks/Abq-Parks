42bd957da66dbdd7ddd4bee20dbd39bcc47861f6
Michael Lacey <laceymwes@gmail.com>
2018-08-01 10:20:22 -0600

    - Initialize MapView in ParkDetailFragment. - Forward MapView lifecycle methods.

--- app/src/main/java/edu/cnm/deepdive/abqparks/fragment/ParkDetailFragment.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/fragment/ParkDetailFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.MapView;
 import edu.cnm.deepdive.abqparks.R;
 
 /**
@@ -15,34 +16,16 @@ import edu.cnm.deepdive.abqparks.R;
  */
 public class ParkDetailFragment extends Fragment {
 
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private static final String ARG_PARAM1 = "param1";
-  private static final String ARG_PARAM2 = "param2";
-
-  // TODO: Rename and change types of parameters
-  private String mParam1;
-  private String mParam2;
-
+  private MapView mapView;
 
   public ParkDetailFragment() {
     // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of this fragment using the provided
-   * parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment ParkDetailFragment.
-   */
-  // TODO: Rename and change types and number of parameters
+  // TODO: Change params to be ParkId. Set parkId argument.
   public static ParkDetailFragment newInstance(String param1, String param2) {
     ParkDetailFragment fragment = new ParkDetailFragment();
     Bundle args = new Bundle();
-    args.putString(ARG_PARAM1, param1);
-    args.putString(ARG_PARAM2, param2);
     fragment.setArguments(args);
     return fragment;
   }
@@ -50,17 +33,51 @@ public class ParkDetailFragment extends Fragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if (getArguments() != null) {
-      mParam1 = getArguments().getString(ARG_PARAM1);
-      mParam2 = getArguments().getString(ARG_PARAM2);
-    }
   }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_park_detail, container, false);
+    View view = inflater.inflate(R.layout.fragment_park_detail, container, false);
+    mapView = view.findViewById(R.id.detail_map_view);
+    return view;
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    mapView.onStart();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
   }
 
 }
