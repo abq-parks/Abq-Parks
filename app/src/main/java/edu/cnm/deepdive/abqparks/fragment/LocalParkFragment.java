4ce3f8e23441328da561cd2312dae2b722b98ea0
adulgr <Dloko505@gmail.com>
2018-08-02 11:24:11 -0600

    Fixed duplicate softball button added adaptor to review fragment.

--- app/src/main/java/edu/cnm/deepdive/abqparks/fragment/LocalParkFragment.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/fragment/LocalParkFragment.java
@@ -180,41 +180,41 @@ public class LocalParkFragment extends Fragment implements OnMapReadyCallback {
   }
 
   // TODO: save postal code to recover from fragment/activity destruction.
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    mapView.onResume();
-  }
-
-  @Override
-  public void onStart() {
-    super.onStart();
-    mapView.onStart();
-  }
-
-  @Override
-  public void onStop() {
-    super.onStop();
-    mapView.onStart();
-  }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    mapView.onPause();
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    mapView.onDestroy();
-  }
+//  @Override
+//  public void onSaveInstanceState(Bundle outState) {
+//    super.onSaveInstanceState(outState);
+//    mapView.onSaveInstanceState(outState);
+//  }
+//
+//  @Override
+//  public void onResume() {
+//    super.onResume();
+//    mapView.onResume();
+//  }
+//
+//  @Override
+//  public void onStart() {
+//    super.onStart();
+//    mapView.onStart();
+//  }
+//
+//  @Override
+//  public void onStop() {
+//    super.onStop();
+//    mapView.onStart();
+//  }
+//
+//  @Override
+//  public void onPause() {
+//    super.onPause();
+//    mapView.onPause();
+//  }
+//
+//  @Override
+//  public void onDestroy() {
+//    super.onDestroy();
+//    mapView.onDestroy();
+//  }
 
   private class ParksAsync extends AsyncTask<Void, Void, List<Park>> {
 
--- app/src/main/java/edu/cnm/deepdive/abqparks/fragment/ReviewDialogFragment.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/fragment/ReviewDialogFragment.java
@@ -8,14 +8,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import com.google.gson.Gson;
 import edu.cnm.deepdive.abqparks.R;
-import edu.cnm.deepdive.abqparks.model.Amenity;
-import edu.cnm.deepdive.abqparks.model.Park;
 import edu.cnm.deepdive.abqparks.model.Review;
 import edu.cnm.deepdive.abqparks.service.ParksService;
 import java.io.IOException;
@@ -29,7 +30,8 @@ public class ReviewDialogFragment extends DialogFragment {
 
   public static final String BASE_URL = "http://10.0.2.2:25052/rest/abq_park/";
 
-  private RecyclerView reviewList;
+  private ReviewAdapter adapter;
+  private RecyclerView recyclerView;
   private EditText reviewText;
 
   private HashMap<String, Integer> reviews;
@@ -93,7 +95,7 @@ public class ReviewDialogFragment extends DialogFragment {
       ParksService client = retrofit.create(ParksService.class);
 
       try {
-        response = client.getReviews().execute();
+        response = client.getReviews(1).execute();
       } catch (IOException e) {
         // Do nothing for now.
       }
@@ -103,11 +105,49 @@ public class ReviewDialogFragment extends DialogFragment {
 
     @Override
     protected void onPostExecute(List<Review> reviews) {
-      ReviewDialogFragment.this.reviews = new HashMap<>();
-      for (Review review : reviews) {
-        ReviewDialogFragment.this.reviews.put(
-            review.getReview(), review.getId());
-      }
+      adapter = new ReviewAdapter(reviews);
+      recyclerView.setAdapter(adapter);
+    }
+  }
+
+  private class ReviewHolder extends RecyclerView.ViewHolder {
+
+    private TextView reviewText;
+
+    public ReviewHolder(LayoutInflater inflater, ViewGroup parent) {
+      super(inflater.inflate(R.layout.review_row, parent, false));
+
+      reviewText = itemView.findViewById(R.id.review_list_text);
+    }
+
+    public void bind(Review review) {
+      reviewText.setText(review.getReview());
+    }
+  }
+
+  private class ReviewAdapter extends RecyclerView.Adapter<ReviewHolder> {
+
+    private List<Review> reviewList;
+
+    public ReviewAdapter(List<Review> review) {
+      reviewList = review;
+    }
+
+    @Override
+    public ReviewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+      LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+      return new ReviewHolder(layoutInflater, viewGroup);
+    }
+
+    @Override
+    public void onBindViewHolder(ReviewHolder reviewHolder, int postion) {
+      Review review = reviewList.get(postion);
+      reviewHolder.bind(review);
+    }
+
+    @Override
+    public int getItemCount() {
+      return reviewList.size();
     }
   }
 }
