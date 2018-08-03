3d61af1756f53c185b72f132b6a5bd5bf20fe50d
Michael Lacey <laceymwes@gmail.com>
2018-08-03 10:57:38 -0600

    - Implement Google Sign client and account encapsulation in ParksApplication class.

--- app/src/main/java/edu/cnm/deepdive/abqparks/ParksApplication.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/ParksApplication.java
@@ -1,8 +1,49 @@
 package edu.cnm.deepdive.abqparks;
 
 import android.app.Application;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class ParksApplication extends Application {
 
+  private static ParksApplication instance;
 
+  GoogleSignInClient signInClient;
+  GoogleSignInAccount singInAccount;
+
+
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    instance = this;
+    GoogleSignInOptions options = new GoogleSignInOptions.Builder()
+        .requestEmail()
+        .requestId()
+        .requestIdToken(getString(R.string.client_id))
+        .build();
+    signInClient = GoogleSignIn.getClient(this, options);
+  }
+
+  public static ParksApplication getInstance() {
+    return instance;
+  }
+
+  public GoogleSignInClient getSignInClient() {
+    return signInClient;
+  }
+
+  public void setSignInClient(GoogleSignInClient signInClient) {
+    this.signInClient = signInClient;
+  }
+
+  public GoogleSignInAccount getSingInAccount() {
+    return singInAccount;
+  }
+
+  public void setSingInAccount(GoogleSignInAccount singInAccount) {
+    this.singInAccount = singInAccount;
+  }
 }
