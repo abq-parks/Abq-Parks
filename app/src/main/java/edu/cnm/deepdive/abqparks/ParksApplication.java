24f91fd2b6ee6839e340cb5842214ada2fd044a0
Michael Lacey <laceymwes@gmail.com>
2018-08-08 14:32:10 -0600

    - Javadoc for ParksApplication.

--- app/src/main/java/edu/cnm/deepdive/abqparks/ParksApplication.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/ParksApplication.java
@@ -6,6 +6,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+/**
+ * Defines logic for Google sign-in.
+ */
 public class ParksApplication extends Application {
 
   private static ParksApplication instance;
@@ -25,22 +28,42 @@ public class ParksApplication extends Application {
     signInClient = GoogleSignIn.getClient(this, options);
   }
 
+  /**
+   * Get instance of ParksApplication.
+   * @return
+   */
   public static ParksApplication getInstance() {
     return instance;
   }
 
+  /**
+   * Get GoogleSignInClient.
+   * @return
+   */
   public GoogleSignInClient getSignInClient() {
     return signInClient;
   }
 
+  /**
+   * Set GoogleSignInClient.
+   * @param signInClient
+   */
   public void setSignInClient(GoogleSignInClient signInClient) {
     this.signInClient = signInClient;
   }
 
+  /**
+   * Get GoogleSignInAccount with profile information.
+   * @return
+   */
   public GoogleSignInAccount getSignInAccount() {
     return signInAccount;
   }
 
+  /**
+   * Set GoogleSignInAccount.
+   * @param signInAccount
+   */
   public void setSignInAccount(GoogleSignInAccount signInAccount) {
     this.signInAccount = signInAccount;
   }
