0b677c688bf697364462364b317eb272b78a3d16
Michael Lacey <laceymwes@gmail.com>
2018-08-08 13:38:16 -0600

    - Javadoc for Amenity model.

--- app/src/main/java/edu/cnm/deepdive/abqparks/model/Amenity.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/model/Amenity.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive.abqparks.model;
 
 import java.util.List;
 
+/**
+ * Park amenity model utilized by Retrofit service.
+ */
 public class Amenity {
 
   private int id;
@@ -12,34 +15,66 @@ public class Amenity {
 
   private List<Park> parks;
 
+  /**
+   * Get amenity ID.
+   * @return Amenity ID.
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Set amenity ID.
+   * @param id
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Get amenity name.
+   * @return
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Set amenity name.
+   * @param name
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Get display name.
+   * @return
+   */
   public String getDisplayName() {
     return displayName;
   }
 
+  /**
+   * Set display name.
+   * @param displayName
+   */
   public void setDisplayName(String displayName) {
     this.displayName = displayName;
   }
 
+  /**
+   * Get list of parks with this amenity.
+   * @return
+   */
   public List<Park> getParks() {
     return parks;
   }
 
+  /**
+   * Set list of parks with this amenity.
+   * @param parks
+   */
   public void setParks(List<Park> parks) {
     this.parks = parks;
   }
