1b30a58f39a7cbf8a6a4369506d7e2c7d61c1b85
Michael Lacey <laceymwes@gmail.com>
2018-08-08 14:28:22 -0600

    - Javadoc for ParksService interface.

--- app/src/main/java/edu/cnm/deepdive/abqparks/service/ParksService.java
+++ app/src/main/java/edu/cnm/deepdive/abqparks/service/ParksService.java
@@ -14,27 +14,65 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+/**
+ * Interface implemented by Retrofit for webservice consumption.
+ */
 public interface ParksService {
 
+  /**
+   * Get all amenities.
+   * @return
+   */
   @GET("amenities")
   Call<List<Amenity>> getAmenities();
 
+  /**
+   * Get parks with user-defined amenities.
+   * @param amenities
+   * @return
+   */
   @GET("parks/{amenities}")
   Call<List<Park>> getParks(@Path("amenities") String amenities);
 
+  /**
+   * Get all parks.
+   * @return
+   */
   @GET("parks")
   Call<List<Park>> getAllParks();
 
+  /**
+   * Get all reviews for user-selected park.
+   * @param parkId
+   * @return
+   */
   @GET("reviews/{parkId}")
   Call<List<Review>> getReviews(@Path("parkId") long parkId);
 
+  /**
+   * Get park matching park ID.
+   * @param parkId
+   * @return
+   */
   @GET("parks/{parkId}")
   Call<Park> getPark(@Path("parkId") long parkId);
 
+  /**
+   * Post user review of park.
+   * @param args
+   * @param review
+   * @return
+   */
   @POST("reviews/{args}")
   @Headers("accept:application/json")
   Call<Review> createReview(@Path("args")String args, @Body Review review);
 
+  /**
+   * Send user credentials to backend for account creation/verification.
+   * @param user
+   * @param authorization
+   * @return
+   */
   @POST("users/")
    Call<User> createUser(@Body User user, @Header("Authorization") String authorization);
 
